8c1121a2cbec52592c0a57b3a4f06cb782b024c2
MarcinRozkwitalski <marcinrozkwitalski98@gmail.com>
2021-01-19 12:58:29 +0100

    Update client.py

--- client.py
+++ client.py
@@ -7,11 +7,30 @@ Created on Sun Jan 17 13:56:49 2021
 
 import socket
 
-c = socket.socket()
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
 
+c = socket.socket()
 c.connect(('localhost', 9999))
 
+keyPair = RSA.generate(3072)
+
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+
 message = input("Enter your message: ")
-c.send(bytes(message, 'utf-8'))
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(message)
+print("Encrypted:", binascii.hexlify(encrypted))
+
+
+c.send(bytes(encrypted, 'utf-8'))
 
 print(c.recv(1024).decode())
