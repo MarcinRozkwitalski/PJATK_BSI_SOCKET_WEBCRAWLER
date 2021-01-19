78938e9ec87c1d04ec1d75676828d95d9b079013
MarcinRozkwitalski <marcinrozkwitalski98@gmail.com>
2021-01-19 13:44:03 +0100

    Added docstring

--- client.py
+++ client.py
@@ -3,6 +3,9 @@
 Created on Sun Jan 17 13:56:49 2021
 
 @author: Marcin
+
+Client side of project, it allows sending encrypted messages 
+by Caesar Cipher Algorithm to server localhost on port 9999.
 """
 
 import socket
@@ -11,10 +14,11 @@ alphabet = "abcdefghijklmnopqrstuvwxyz "
 letter_to_index = dict(zip(alphabet, range(len(alphabet))))
 index_to_letter = dict(zip(range(len(alphabet)), alphabet))
 
-def encrypt(message, shift=3):
+def encrypt(message, shift):
     cipher = ""
-
+    # transverse the plain text
     for letter in message:
+        # change letters to index by given shift
         number = (letter_to_index[letter] + shift) % len(letter_to_index)
         letter = index_to_letter[number]
         cipher += letter
@@ -22,12 +26,16 @@ def encrypt(message, shift=3):
     return cipher
         
 def main():
+    # do socket function on client (c)
     c = socket.socket()
+    # connect to host named 'localhost' with port '9999'
     c.connect(('localhost', 9999))
     message = input("Enter message: ")
-    encrypted_message = encrypt(message, shift=3)
+    encrypted_message = encrypt(message, 3)
     print("Encrypted message:",encrypted_message)
+    # client sends message by utf-8 encoding
     c.send(bytes(encrypted_message, 'utf-8'))
+    # print received message from server
     print(c.recv(1024).decode())
 
 main()
--- server.py
+++ server.py
@@ -3,24 +3,26 @@
 Created on Sun Jan 17 14:13:59 2021
 
 @author: Marcin
+
+Server side of project, it listens on port 9999.
+It allows receiving encrypted messages by Caesar Cipher Algorithm.
 """
 
 import socket
 
-
 s = socket.socket()
 print('Socket Created')
-
+# create host named 'localhost' with port '9999'
 s.bind(('localhost', 9999))
-
 s.listen(3)
-print('waiting for connections')
+print('Waiting for connections...')
 
 while True:
     c, addr = s.accept()
+    # decoding received message from client
     message = c.recv(1024).decode()
     print("Connected with ", addr, message)
-    
+    # send to client 
     c.send(bytes('Server has successfully received your message','utf-8'))
-    
+    # close connection to client
     c.close()
